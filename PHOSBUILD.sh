625bcb7faa4a1f63221e877aa388b6d97752db69
The Phantom Technology -Advanced Pentest <155259033+thephantomtechnology@users.noreply.github.com>
2025-01-21 16:44:37 +0800

    更新 PHOSBUILD.sh

--- PHOSBUILD.sh
+++ PHOSBUILD.sh
@@ -1,11 +1,10 @@
+rm -rf /opt/PHANTX/Impulse
+
 rm -rf .git*
+
+pip3 install -r requirements.txt
+echo "Pip Install... PASS!"
 
 cp -Rf $(pwd) /opt/PHANTX/impulse
 
-WORKDIR=$(pwd)
-
-cd /opt/PHANTX/impulse
-
-pip3 install -r requirements.txt
-
-cd $WORKDIR
+echo "Copy PACKAGE... PASS!"
